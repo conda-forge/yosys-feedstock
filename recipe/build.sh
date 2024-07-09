fb3fde494e652a285c78d531ff1cad279e74cef7
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-07-09 12:37:50 -0400

    clone abc now that its a submodule

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,13 @@
 set -e
 set -x
 
+# clone abc now that its a submodule
+git clone git@github.com:YosysHQ/abc.git
+pushd abc
+# NOTE: this should match in yosys
+git checkout 237d81397fcc85dd3894bf1a449d2955cd3df02d
+popd
+
 if [[ "${target_platform}" == "linux-64" ]]; then
     # expects `gcc`
     ln -s $GCC $BUILD_PREFIX/bin/gcc
