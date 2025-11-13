7eef0fe60ee3c5db53e1e2a4057fd4ddc99bb20b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-13 11:28:39 -0500

    Simplify build.sh for linux-64 platform
    Removed unnecessary comments and simplified the build process for linux-64.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,7 @@ set -e
 set -x
 
 if [[ "${target_platform}" == "linux-64" ]]; then
-    # expects `gcc`
-    # ln -s $GCC $BUILD_PREFIX/bin/gcc
-
     make CONFIG=gcc -j $(nproc)
-
 else
     make CONFIG=clang -j $(sysctl -n hw.physicalcpu)
     
